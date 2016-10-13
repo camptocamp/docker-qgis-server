5630b34754c65cf138eed9123fe4d00248e58cc7
Patrick Valsecchi <patrick.valsecchi@camptocamp.com>
2016-10-13 13:31:30 +0200

    Add the docker logs to the acceptance test output
    This is not 100% perfect because the docker-compose logs output may have a
    small delay. So the output of the logs from the previous test may show up
    on the next test. But this is the best we can provide without adding a weird
    sleep between tests.

--- acceptance_tests/__init__.py
+++ acceptance_tests/__init__.py
@@ -3,6 +3,7 @@ import os
 import psycopg2
 import requests
 import subprocess
+import sys
 import time
 from xml.etree import ElementTree
 
@@ -12,7 +13,7 @@ from acceptance_tests import utils
 BASE_URL = 'http://host:8380/' if utils.in_docker() else 'http://localhost:8380/'
 DB_ADDR = 'host' if utils.in_docker() else 'localhost'
 LOG = logging.getLogger(__name__)
-logging.basicConfig(level=logging.DEBUG, format="%(asctime)-15s %(levelname)5s %(name)s %(message)s")
+logging.basicConfig(level=logging.DEBUG, format="TEST                 | %(asctime)-15s %(levelname)5s %(name)s %(message)s", stream=sys.stdout)
 logging.getLogger("requests.packages.urllib3.connectionpool").setLevel(logging.WARN)
 PROJECT_NAME='qgis'
 
@@ -25,14 +26,23 @@ class Composition(object):
             request.addfinalizer(self.stop_all)
         if os.environ.get("docker_start", "1") == "1":
             subprocess.check_call(['docker-compose', '--file', composition,
-                                   '--project-name', PROJECT_NAME, 'rm', '-f'], env=env)
+                                   '--project-name', PROJECT_NAME, 'rm', '-f'], env=env,
+                                  stderr=subprocess.STDOUT)
 
             # to rebuild testDB, if needed
             subprocess.check_call(['docker-compose', '--file', composition,
-                                   '--project-name', PROJECT_NAME, 'build'], env=env)
+                                   '--project-name', PROJECT_NAME, 'build'], env=env,
+                                  stderr=subprocess.STDOUT)
 
             subprocess.check_call(['docker-compose', '--file', composition,
-                                   '--project-name', PROJECT_NAME, 'up', '-d'], env=env)
+                                   '--project-name', PROJECT_NAME, 'up', '-d'], env=env,
+                                  stderr=subprocess.STDOUT)
+
+        # setup something that redirects the docker container logs to the test output
+        log_watcher = subprocess.Popen(['docker-compose', '--file', composition,
+                                       '--project-name', PROJECT_NAME, 'logs', '--follow', '--no-color'],
+                                       env=env, stderr=subprocess.STDOUT)
+        request.addfinalizer(log_watcher.kill)
 
         #QGIS server is a bit stupid. If the DB is not there during the first query, it will cache the
         #project without the DB layers for ever. So we have to wait for the DB to be up before quering QGIS
@@ -43,15 +53,18 @@ class Composition(object):
 
     def stop_all(self):
         subprocess.check_call(['docker-compose', '--file', self.composition,
-                               '--project-name', PROJECT_NAME, 'stop'], env=Composition._get_env())
+                               '--project-name', PROJECT_NAME, 'stop'], env=Composition._get_env(),
+                              stderr=subprocess.STDOUT)
 
     def stop(self, container):
         subprocess.check_call(['docker', '--log-level=warn',
-                               'stop', '%s_%s_1' % (PROJECT_NAME, container)])
+                               'stop', '%s_%s_1' % (PROJECT_NAME, container)],
+                              stderr=subprocess.STDOUT)
 
     def restart(self, container):
         subprocess.check_call(['docker', '--log-level=warn',
-                               'restart', '%s_%s_1' % (PROJECT_NAME, container)])
+                               'restart', '%s_%s_1' % (PROJECT_NAME, container)],
+                              stderr=subprocess.STDOUT)
 
     @staticmethod
     def _get_env():
@@ -78,7 +91,7 @@ def wait_db():
             break
         except:
             assert time.time() <= timeout
-            time.sleep(0.2)
+            time.sleep(0.5)
 
 def wait_mapserver():
     timeout = time.time() + 60.0
