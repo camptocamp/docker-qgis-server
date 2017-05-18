adffb7219eee695088c0f01cc1fd6f13ca921333
Patrick Valsecchi <patrick.valsecchi@camptocamp.com>
2017-05-18 09:22:14 +0200

    Fix build

--- builder/build.sh
+++ builder/build.sh
@@ -3,13 +3,17 @@ set -e
 
 cd /build
 
+export CXX="clang++"
+export CC="clang"
+
 cmake /src \
       -GNinja \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_INSTALL_PREFIX=/usr/local \
       -DWITH_DESKTOP=ON \
       -DWITH_SERVER=ON \
-      -DBUILD_TESTING=OFF  \
+      -DBUILD_TESTING=OFF
 
+ccache -M10G
 ninja install
 ccache -s
