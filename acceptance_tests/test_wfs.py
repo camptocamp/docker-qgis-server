d1e50e179319db594f9952d970c5acc36c6ea4e9
Patrick Valsecchi <patrick.valsecchi@camptocamp.com>
2018-05-02 08:18:46 +0200

    Switch to c2cwsgiutils:2

--- acceptance_tests/test_wfs.py
+++ acceptance_tests/test_wfs.py
@@ -1,9 +1,10 @@
+from c2cwsgiutils.acceptance.connection import CacheExpected
 from xml.etree import ElementTree
 
 
 def test_get_feature(connection):
     answer = connection.get_xml('?SERVICE=WFS&VERSION=2.0.0&REQUEST=GetFeature&TYPENAME=polygons',
-                                cache_allowed=True)
+                                cache_expected=CacheExpected.DONT_CARE)
     ns = '{http://www.qgis.org/gml}'
     features = answer.findall(".//%spolygons" % ns)
     assert len(features) == 1, ElementTree.dump(answer)
