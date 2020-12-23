a085429378d9a3c40d52fa8ab5b032af33a251e8
Andrea Borghi <andrea.borghi@camptocamp.com>
2020-12-23 15:00:14 +0100

    fix checks...

--- acceptance_tests/test_wfs.py
+++ acceptance_tests/test_wfs.py
@@ -1,11 +1,14 @@
-from c2cwsgiutils.acceptance.connection import CacheExpected
 from xml.etree import ElementTree
+
+from c2cwsgiutils.acceptance.connection import CacheExpected
 
 
 def test_get_feature(connection):
-    answer = connection.get_xml('?SERVICE=WFS&VERSION=1.0.0&REQUEST=GetFeature&TYPENAME=polygons',
-                                cache_expected=CacheExpected.DONT_CARE)
-    ns = '{http://www.qgis.org/gml}'
+    answer = connection.get_xml(
+        "?SERVICE=WFS&VERSION=1.0.0&REQUEST=GetFeature&TYPENAME=polygons",
+        cache_expected=CacheExpected.DONT_CARE,
+    )
+    ns = "{http://www.qgis.org/gml}"
     features = answer.findall(".//%spolygons" % ns)
     assert len(features) == 1, ElementTree.dump(answer)
-    assert [e.text for e in answer.findall(".//%spolygons/%sname" % (ns, ns))] == ['foo']
+    assert [e.text for e in answer.findall(".//%spolygons/%sname" % (ns, ns))] == ["foo"]
--- acceptance_tests/test_wms.py
+++ acceptance_tests/test_wms.py
@@ -1,23 +1,36 @@
-from c2cwsgiutils.acceptance.connection import CacheExpected
 from xml.etree import ElementTree
+
+from c2cwsgiutils.acceptance.connection import CacheExpected
 
 
 def test_get_capabilities(connection):
-    ns = '{http://www.opengis.net/wms}'
-    answer = connection.get_xml("?SERVICE=WMS&REQUEST=GetCapabilities&VERSION=1.3.0", cache_expected=CacheExpected.DONT_CARE)
-    assert [e.text for e in answer.findall("%sService/%sTitle" % (ns, ns))] == ['test'], ElementTree.dump(answer)
-    assert [e.text for e in answer.findall(".//%sLayer/%sName" % (ns, ns))] == ['test', 'polygons'], ElementTree.dump(answer)
+    ns = "{http://www.opengis.net/wms}"
+    answer = connection.get_xml(
+        "?SERVICE=WMS&REQUEST=GetCapabilities&VERSION=1.3.0", cache_expected=CacheExpected.DONT_CARE
+    )
+    assert [e.text for e in answer.findall("%sService/%sTitle" % (ns, ns))] == ["test"], ElementTree.dump(
+        answer
+    )
+    assert [e.text for e in answer.findall(".//%sLayer/%sName" % (ns, ns))] == [
+        "test",
+        "polygons",
+    ], ElementTree.dump(answer)
 
 
 def test_get_map(connection):
-    answer = connection.get_raw("?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&LAYERS=polygons&STYLES=&"
-                                "CRS=EPSG:4326&BBOX=-180,-90,180,90&WIDTH=600&HEIGHT=300&FORMAT=image/png",
-                                cache_expected=CacheExpected.DONT_CARE)
-    if answer.headers["content-type"] != 'image/png':
+    answer = connection.get_raw(
+        "?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&LAYERS=polygons&STYLES=&"
+        "CRS=EPSG:4326&BBOX=-180,-90,180,90&WIDTH=600&HEIGHT=300&FORMAT=image/png",
+        cache_expected=CacheExpected.DONT_CARE,
+    )
+    if answer.headers["content-type"] != "image/png":
         print(answer.text)
-    assert answer.headers["content-type"] == 'image/png'
+    assert answer.headers["content-type"] == "image/png"
+
 
 def test_other_url(connection):
-    connection.get("toto/tutu?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&LAYERS=polygons&STYLES="
-                   "&CRS=EPSG:4326&BBOX=-180,-90,180,90&WIDTH=600&HEIGHT=300&FORMAT=image/png",
-                   cache_expected=CacheExpected.DONT_CARE)
+    connection.get(
+        "toto/tutu?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&LAYERS=polygons&STYLES="
+        "&CRS=EPSG:4326&BBOX=-180,-90,180,90&WIDTH=600&HEIGHT=300&FORMAT=image/png",
+        cache_expected=CacheExpected.DONT_CARE,
+    )
